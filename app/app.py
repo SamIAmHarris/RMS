0e21a2a2457cbd52235d7923facdae218833c516
SamIAmHarris <samharris512@gmail.com>
2025-02-28 16:49:44 -0500

    Fix PORT deploy, but we won't use that I think

--- app/app.py
+++ app/app.py
@@ -43,4 +43,4 @@ def get_product(product_id):
     return jsonify({"error": "Product not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
